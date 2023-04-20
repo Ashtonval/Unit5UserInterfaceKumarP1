ad07adac78b5be0a6c27437eaa0f62b0774f9e9a
Ashtonval <tennisjedi20xx@gmail.com>
2023-04-20 09:53:13 -0700

    Completed 5.2 and started 5.3
    Got the score and gameover text to appear

--- Unit5UserInterface/Assets/Scripts/Target.cs
+++ Unit5UserInterface/Assets/Scripts/Target.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    public int pointValue;
     private Rigidbody targetRb;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    private GameManager gameManager;
+    public ParticleSystem explosionParticle;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +20,7 @@ public class Target : MonoBehaviour
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
     Vector3 RandomForce()
     {
@@ -32,11 +36,20 @@ public class Target : MonoBehaviour
     }
     private void OnMouseDown()
     {
-        Destroy(gameObject);
+        if(gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }    
     }
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
 
     // Update is called once per frame
